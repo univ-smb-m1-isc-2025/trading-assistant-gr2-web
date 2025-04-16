f5fb27367afecf0673aa5b05dc4ad095868ed56c
zCaps <gabriel.abrantes-alfredo@etu.univ-smb.fr>
2025-04-16 12:38:31 +0200

    Google auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const googleClientId = "156580473624-avdc8usdprcdv0h31hahk6sg0ipmcqn1.apps.googleusercontent.com"
+
+if (!googleClientId || googleClientId === "156580473624-avdc8usdprcdv0h31hahk6sg0ipmcqn1.apps.googleusercontent.com") {
+  console.warn("Attention: L'ID Client Google n'est pas configuré dans main.tsx ! La connexion Google ne fonctionnera pas."); 
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <GoogleOAuthProvider clientId={googleClientId}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
